272111d378a6ba18474842180ddeb43e82ef6112
ndroock1 <ndroock1@gmail.com>
2022-04-21 13:37:25 +0200

    Various

---
+++ JavaMMaPT/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		List<String> items = List.of("1", "a", "2", "a", "3", "a");
+		items.forEach(item -> {
+			if (item.equals("a")) {
+				System.out.println("A");
+			} else {
+				System.out.println("Not A");
+			}
+		});
+	}
+}
